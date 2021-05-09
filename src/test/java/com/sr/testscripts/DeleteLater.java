3a5a9ae58b1c797d5120c709eb070151430fb2a6
Superuser <Superuser@DESKTOP-SFEA06R>
2021-05-09 09:40:45 +0530

    Added new file

---
+++ src/test/java/com/sr/testscripts/DeleteLater.java
@@ -0,0 +1,11 @@
+package com.sr.testscripts;
+
+import org.testng.annotations.Test;
+
+public class DeleteLater {
+
+		@Test
+		public void a() {
+			System.out.println("Hello 9 May 09:39");
+		}
+}
